f36ac1b5e21246c2b2a12301cfa21c21c51c244d
Andie <oestefa23@gmail.com>
2026-06-16 15:48:57 -0500

    arquitectura del sistema

---
+++ apps/automatizacion/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AutomatizacionConfig(AppConfig):
+    name = 'apps.automatizacion'
---
+++ apps/ocupacion/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OcupacionConfig(AppConfig):
+    name = 'apps.ocupacion'
